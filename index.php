b7cf76cf9cf5f6ee4b7dcbd510718c6badc0f9d9
Fhsdmh <ashifahmadhakim@gmail.com>
2020-01-01 23:30:55 +0700

    desain3

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" id="home">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -16,8 +16,8 @@
     <!-- awal navbar -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #f6f6f6;">
       <div class="container">
-        <a class="navbar-brand" href="#">
-          <img src="images/logonav.svg" alt="logo navbar" width="82" height="47" style="margin-right: 150px;">
+        <a class="navbar-brand" href="#home" style="margin-right: 150px;">
+          <img src="images/logonav.svg" alt="logo navbar" width="82" height="47">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -26,13 +26,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active" style="padding-right: 50px;">
-              <a class="nav-link" href="" style="color: #0c1e2d;">Beranda</a>
+              <a class="nav-link" href="#home" style="color: #0c1e2d;">Beranda</a>
             </li>
             <li class="nav-item active" style="padding-right: 50px;">
-              <a class="nav-link" href="" style="color: #0c1e2d;">Daftar Harga</a>
+              <a class="nav-link" href="#daftarharga" style="color: #0c1e2d;">Daftar Harga</a>
             </li>
             <li class="nav-item active" style="padding-right: 50px;">
-              <a class="nav-link" href="" style="color: #0c1e2d;">Hubungi Kami</a>
+              <a class="nav-link" href="#contactus" style="color: #0c1e2d;">Hubungi Kami</a>
             </li>
             <li class="nav-item active"style="padding-right: 50px;">
               <a class="nav-link" href="" style="color: #0c1e2d;">Tentang</a>
@@ -122,6 +122,7 @@
     </div>
     <!-- akhir modal lupa password -->
 
+    <!-- awal modal daftar -->
       <div class="modal fade bd-example-modal-md" id="exampleModalCenter3" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle3" aria-hidden="true">
         <div class="modal-dialog modal-md modal-dialog-scrollable" role="document">
           <div class="modal-content">
@@ -153,7 +154,7 @@
 
                 <div class="form-group">
                   <label for="formGroupExampleInput7">Alamat lengkap</label>
-                  <input type="text" class="form-control" placeholder="Masukkan Alamat lengkap Anda" required oninvalid="this.setCustomValidity('Mohon masukkan Alamat lengkap Anda!')" oninput="setCustomValidity('')">
+                  <textarea class="form-control" placeholder="Masukkan Alamat lengkap Anda" required oninvalid="this.setCustomValidity('Mohon masukkan Alamat lengkap Anda!')" oninput="setCustomValidity('')"></textarea>
                   <small id="emailHelp" class="form-text text-muted">Mohon sertakan juga RT/RW Anda</small>
                 </div>
 
@@ -181,6 +182,75 @@
           </div>
         </div>
       </div>
+      <!-- akhir modal daftar -->
+
+
+      <!-- awal modal mulai jual -->
+      <div class="modal fade bd-example-modal-md" id="exampleModalCenter4" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle4" aria-hidden="true">
+        <div class="modal-dialog modal-md modal-dialog-scrollable" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+              </button>
+              <h5 class="modal-title" id="exampleModalCenterTitle">RongsokID</h5>
+              <h6 class="modal-title2">Satu akun untuk semua akses</h6>
+            </div>
+
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="formGroupExampleInput10">Nama Lengkap</label>
+                  <input type="text" class="form-control" placeholder="Masukkan Nama lengkap Anda" required oninvalid="this.setCustomValidity('Mohon masukkan Nama lengkap Anda!')" oninput="setCustomValidity('')">
+                </div>
+
+                <div class="form-group">
+                  <label for="formGroupExampleInput11">No. Telepon</label>
+                  <input type="number" class="form-control" placeholder="Masukkan No. telepon Anda" required oninvalid="this.setCustomValidity('Mohon masukkan No. telepon Anda!')" oninput="setCustomValidity('')">
+                </div>
+
+                <div class="form-group">
+                  <label for="formGroupExampleInput12">Email</label>
+                  <input type="email" class="form-control" placeholder="Masukkan Email Anda" required oninvalid="this.setCustomValidity('Mohon masukkan Email Anda!')" oninput="setCustomValidity('')">
+                  <small id="emailHelp" class="form-text text-muted">Kami tidak akan memberi tahu siapapun tentang Email Anda</small>
+                </div>
+
+                <div class="form-group">
+                  <label for="formGroupExampleInput13">Alamat lengkap</label>
+                  <textarea class="form-control" placeholder="Masukkan Alamat lengkap Anda" required oninvalid="this.setCustomValidity('Mohon masukkan Alamat lengkap Anda!')" oninput="setCustomValidity('')"></textarea>
+                  <small id="emailHelp" class="form-text text-muted">Mohon sertakan juga RT/RW Anda</small>
+                </div>
+
+                <div class="form-group">
+                  <label for="formGroupExampleInput14">Jenis barang bekas</label>
+                  <div class="input-group mb-3">
+                  <select class="custom-select" id="inputGroupSelect01" required oninvalid="this.setCustomValidity('Mohon pilih Jenis barang bekas Anda!')" oninput="setCustomValidity('')">
+                    <option value="" disable selected>Pilih jenis barang bekas Anda</option>
+                    <option value="1">Bahan plastik</option>
+                    <option value="2">Bahan besi</option>
+                    <option value="3">Kertas HVS</option>
+                    <option value="4">Kertas lainnya</option>
+                    <option value="5">Campuran</option>
+                    <option value="6">Lainnya</option>
+                  </select>
+                  </div>
+                </div>
+
+                <div class="modal-body2">
+                  <p style="margin-bottom: 0px;">Dengan klik Jemput, kamu menyetujui</p>
+                  <p style="margin-bottom: 20px;"><a href="index.php">Syarat dan Ketentuan</a> yang telah ditentukan oleh <a href="index.php"><img src="images/katarongsokkecil9.jpg" style="vertical-align: sub;"></a></p>
+                  <button type="submit" class="btn">JEMPUT</button>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer" style="padding-bottom: 0px;">
+              <p>Sudah memiliki akun? <button type="button" class="btn btn-link" data-dismiss="modal" data-toggle="modal" data-target="#exampleModalCenter" aria-hidden="true">Masuk</button></p>
+            </div>
+            <p>Belum memiliki akun? <button type="button" class="btn btn-link" data-dismiss="modal" data-toggle="modal" data-target="#exampleModalCenter3" aria-hidden="true">Daftar</button></p>
+          </div>
+        </div>
+      </div>
+      <!-- akhir modal mulai jual -->
 
 
     <!-- awal jumbotron -->
@@ -192,11 +262,10 @@
       <h1>lebih berkelas</h1>
       <h1>di mana pun</h1>
       <h1 style="margin-bottom: 14px;">kapan pun.</h1>
-      <p>Rongsok merupakan platform yang menyediakan sarana</p>
+      <p><img src="images/katarongsokkecil9.jpg"> merupakan platform yang menyediakan sarana</p>
       <p style="margin-bottom: 25px;">untuk memudahkan kamu dalam menjual barang bekas (rongsok)</p>
-      <a class="btn btn-primary" href="mulaijual.php" role="button">Mulai Jual</a>
-      </button>
-      </div>
+      <button type="button" class="btn" data-toggle="modal" data-target="#exampleModalCenter4">Mulai Jual</button>
+    </div>
 
       <div class="col-sm-4">
         <img src="images/animasijumbotron.svg" alt="animasi jumbo tron">
@@ -293,5 +362,7 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <script src="js/script.js"></script>
   </body>
 </html>
